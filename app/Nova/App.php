d1231ac0061134f940f8f2c9315b354382c09632
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2026-05-20 15:55:43 +0200

    feat: add App resource and translations in multiple languages OC:7757
    Introduce a new App resource extending WmNovaApp in the Nova namespace, featuring relatable user queries for Administrators and creation authorization through SuperAdminService. Additionally, include translations for the new App resource in German, English, Spanish, French, Italian, Dutch, and Albanian to improve localization support.

--- app/Nova/App.php
+++ app/Nova/App.php
@@ -2,6 +2,26 @@
 
 namespace App\Nova;
 
+use Illuminate\Http\Request;
+use Laravel\Nova\Http\Requests\NovaRequest;
+use Wm\WmPackage\Models\User;
 use Wm\WmPackage\Nova\App as WmNovaApp;
+use Wm\WmPackage\Support\SuperAdminService;
 
-class App extends WmNovaApp {}
+class App extends WmNovaApp
+{
+    public static function relatableUsers(NovaRequest $request, $query)
+    {
+        return $query->role('Administrator');
+    }
+
+    public static function authorizedToCreate(Request $request): bool
+    {
+        return SuperAdminService::allows($request);
+    }
+
+    public static function createButtonLabel(): string
+    {
+        return __('Create App');
+    }
+}
