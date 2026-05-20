eb98e7a35d12bd65a853a6d9bfa4ba268c3cc154
Rubens Garofalo <rubensgiuseppegarofalo@gmail.com>
2026-05-20 11:35:48 +0200

    chore(nova): remove regeneration actions OC:7911_7912
    Removed BuildAppPoisGeojsonAction and GenerateAppIconsAction for app optimization. Includes deletion of associated language support.

--- app/Nova/App.php
+++ app/Nova/App.php
@@ -4,8 +4,6 @@ namespace App\Nova;
 
 use Laravel\Nova\Fields\BelongsTo;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use App\Nova\Actions\BuildAppPoisGeojsonAction;
-use App\Nova\Actions\GenerateAppIconsAction;
 use Wm\WmPackage\Nova\App as WmNovaApp;
 
 class App extends WmNovaApp
@@ -23,27 +21,5 @@ class App extends WmNovaApp
         array_splice($fields, 1, 0, [$userField]);
 
         return $fields;
-    }
-
-    public function actions(NovaRequest $request): array
-    {
-        $teamOnly = fn (NovaRequest $request) => optional($request->user())->email === 'team@webmapp.it';
-
-        return array_merge(parent::actions($request), [
-            (new BuildAppPoisGeojsonAction)
-                ->onlyOnDetail()
-                ->canSee($teamOnly)
-                ->canRun($teamOnly)
-                ->confirmText(__('Are you sure you want to regenerate pois.geojson for this app? The job will be queued and may take a while.'))
-                ->confirmButtonText(__('Yes, regenerate'))
-                ->cancelButtonText(__('Cancel')),
-            (new GenerateAppIconsAction)
-                ->onlyOnDetail()
-                ->canSee($teamOnly)
-                ->canRun($teamOnly)
-                ->confirmText(__('Are you sure you want to regenerate icons.json for this app? This operation may take a while.'))
-                ->confirmButtonText(__('Yes, regenerate'))
-                ->cancelButtonText(__('Cancel')),
-        ]);
     }
 }
